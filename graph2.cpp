db212c0a241159b1a7f66e11e42c9e1fa4cd4daa
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-10-24 09:26:39 +0530

    Add files via upload

---
+++ graph2.cpp
@@ -0,0 +1,43 @@
+//547. Number of Provinces (leetcode)
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& adjlist) {
+        vis[node] = 1;
+        for (auto item : adjlist[node]) {
+            if (!vis[item]) {
+                dfs(item, vis, adjlist);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adjlist(n);  // ✅ use vector of vector
+
+        // Build adjacency list
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adjlist[i].push_back(j);
+                    adjlist[j].push_back(i);
+                }
+            }
+        }
+
+        vector<int> vis(n, 0);
+        int count = 0;  // ✅ define count
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                count++;
+                dfs(i, vis, adjlist);
+            }
+        }
+
+        return count;
+    }
+};
+
---
+++ graph3.cpp
@@ -0,0 +1,29 @@
+//733. Flood Fill (leetcode)
+#include <iostream>
+using namespace std;
+class Solution {
+private:
+    void dfs(vector<vector<int>>& image,int inicolor,vector<vector<int>>&ans,int delrow[],int delcol[],int sr,int sc, int newcolor){
+        ans[sr][sc]=newcolor;
+        int n=image.size();
+        int m=image[0].size();
+        for(int i=0;i<4;i++){
+            int nrow=sr+delrow[i];
+            int ncol=sc +delcol[i];
+            if(nrow >=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==inicolor && ans[nrow][ncol]!= newcolor){
+                ans[nrow][ncol]=newcolor;
+                dfs(image,inicolor,ans,delrow,delcol,nrow,ncol,newcolor);
+            }
+        }
+    }    
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int inicolor=image[sr][sc];
+        vector<vector<int>>ans=image;
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+        dfs(image,inicolor,ans,delrow,delcol,sr,sc,color);
+        return ans;
+        
+    }
+};
