f94364adde423ffb5ad84fd44b2f6efcd9cfc7dd
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-10-25 09:40:32 +0530

    Add files via upload

---
+++ graph4.cpp
@@ -0,0 +1,37 @@
+//Shortest Path in Undirected Graph
+#include<iostream>
+using namespace std;
+class Solution {
+  public:
+    // Function to find the shortest path from source to all other nodes
+    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+        // code here
+        int n=adj.size();
+        
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        
+        queue<int>q;
+        q.push(src);
+        while(!q.empty()){
+            int node =q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                if(dist[node]+1<dist[it]){
+                    dist[it]=dist[node]+1;
+                    q.push(it);
+                }
+            }
+        }
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            if(dist[i]!= 1e9){
+                ans[i]=dist[i];
+            }
+            else if(dist[i]==1e9){
+                ans[i]=-1;
+            }
+        }
+        return ans;
+    }
+};
