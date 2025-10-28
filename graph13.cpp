a7505ac519e7b00d8bf91b1d0a0290ea658541dd
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-10-28 18:56:07 +0530

    Add files via upload

---
+++ graph13.cpp
@@ -0,0 +1,31 @@
+// Minimum Multiplications to reach End 
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        // code here
+        if (start == end) return 0; 
+        queue<pair<int,int>>q;
+        vector<int>dist(100000,1e9);
+        q.push({start,0});
+        dist[start]=0;
+        int mod=100000;
+        while(!q.empty()){
+            int node=q.front().first;
+            int steps=q.front().second;
+            q.pop();
+            
+            for(auto it: arr){
+                int num=(it*node)%mod;
+                if(steps+1 < dist[num]){
+                    dist[num]=steps+1;
+                    if(num==end) return steps+1;
+                    q.push({num,dist[num]});
+                }
+            }
+        }
+        return -1;
+    }
+};
